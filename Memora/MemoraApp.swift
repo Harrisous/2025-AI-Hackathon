80ca78b4a7b8bf32890883c3d1233494769e74c3
Wruiiiii <ruiwang025@gmail.com>
2025-11-07 22:49:01 -0500

    Initial Commit

---
+++ Memora/MemoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoraApp.swift
+//  Memora
+//
+//  Created by Rae Wang on 11/7/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
